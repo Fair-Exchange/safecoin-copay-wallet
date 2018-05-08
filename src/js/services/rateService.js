b722f08012229752ae3ae1a2faa58a2178ca91df
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-08 14:30:09 +0300

    Update rateService.js

--- src/js/services/rateService.js
+++ src/js/services/rateService.js
@@ -17,8 +17,8 @@ var RateService = function(opts) {
   self.httprequest = opts.httprequest; // || request;
   self.lodash = opts.lodash;
 
-  self.SAT_TO_XSG = 1 / 1e8;
-  self.XSG_TO_SAT = 1e8;
+  self.SAT_TO_SAFE = 1 / 1e8;
+  self.SAFE_TO_SAT = 1e8;
   self.UNAVAILABLE_ERROR = 'Service is not available - check for service.isAvailable() or use service.whenAvailable()';
   self.UNSUPPORTED_CURRENCY_ERROR = 'Currency not supported';
 
@@ -45,12 +45,12 @@ RateService.prototype.updateRates = function() {
 
   var backoffSeconds = 5;
   var updateFrequencySeconds = 5 * 60;
-  var rateServiceUrl = 'https://rates.snowgem.org/rates';
+  var rateServiceUrl = 'https://rates.safecoin.org/rates'; //TODO
 
-  function getXSG(cb, tries) {
+  function getSAFE(cb, tries) {
     tries = tries || 0;
     if (!self.httprequest) return;
-    if (tries > 5) return cb('could not get XSG rates');
+    if (tries > 5) return cb('could not get SAFE rates');
 
     //log.info('Fetching exchange rates');
     self.httprequest.get(rateServiceUrl).success(function(res) {
@@ -68,13 +68,13 @@ RateService.prototype.updateRates = function() {
       //log.debug('Error fetching exchange rates', err);
       setTimeout(function() {
         backoffSeconds *= 1.5;
-        getXSG(cb, tries++);
+        getSAFE(cb, tries++);
       }, backoffSeconds * 1000);
       return;
     })
   }
 
-  getXSG(function(err) {
+  getSAFE(function(err) {
     if (err) return;
     self._isAvailable = true;
     self.lodash.each(self._queued, function(callback) {
@@ -110,14 +110,14 @@ RateService.prototype.toFiat = function(satoshis, code, chain) {
     return null;
   }
 
-  return satoshis * this.SAT_TO_XSG * this.getRate(code, chain);
+  return satoshis * this.SAT_TO_SAFE * this.getRate(code, chain);
 };
 
 RateService.prototype.fromFiat = function(amount, code, chain) {
   if (!this.isAvailable()) {
     return null;
   }
-  return amount / this.getRate(code, chain) * this.XSG_TO_SAT;
+  return amount / this.getRate(code, chain) * this.SAFE_TO_SAT;
 };
 
 RateService.prototype.listAlternatives = function(sort) {
