7b36234ccf7bafa3f37537ba321394f17e3f60d0
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-08 13:55:42 +0300

    Update preferencesAltCurrency.js

--- src/js/controllers/preferencesAltCurrency.js
+++ src/js/controllers/preferencesAltCurrency.js
@@ -10,7 +10,7 @@ angular.module('copayApp.controllers').controller('preferencesAltCurrencyControl
       var unusedCurrencyList = [{
         isoCode: 'LTL'
       }, {
-        isoCode: 'XSG'
+        isoCode: 'SAFE'
       }];
       rateService.whenAvailable(function() {
 
