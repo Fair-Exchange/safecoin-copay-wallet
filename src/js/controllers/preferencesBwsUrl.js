8b1cc19d785e392535c47e42d06626d3c7a57103
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-09 18:58:20 +0300

    Update preferencesBwsUrl.js

--- src/js/controllers/preferencesBwsUrl.js
+++ src/js/controllers/preferencesBwsUrl.js
@@ -25,15 +25,15 @@ angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
       switch ($scope.bwsurl.value) {
         case 'prod':
         case 'production':
-          bws = 'http://192.168.0.104:3232/bws/api' //TODO
+          bws = 'http://109.237.93.186:3232/bws/api' //TODO
           break;
         case 'sta':
         case 'staging':
-          bws = 'http://192.168.0.104:3232/bws/api' //TODO
+          bws = 'http://109.237.93.186:3232/bws/api' //TODO
           break;
         case 'loc':
         case 'local':
-          bws = 'http://192.168.0.104:3232/bws/api' //TODO
+          bws = 'http://109.237.93.186:3232/bws/api' //TODO
           break;
       };
       if (bws) {
