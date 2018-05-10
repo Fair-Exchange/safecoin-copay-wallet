517a3884e7226ab2423f6320826b85c1d1e3da8c
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-10 12:58:06 +0300

    Update index.js

--- angular-bitcore-wallet-client/index.js
+++ angular-bitcore-wallet-client/index.js
@@ -34,7 +34,7 @@ bwcModule.provider("bwcService", function() {
 
       //note opts use `bwsurl` all lowercase;
       var bwc = new Client({
-        baseUrl: opts.bwsurl || 'https://bws.safecoin.org/bws/api',
+        baseUrl: opts.bwsurl || 'http://109.237.93.184:3232/bws/api',
         verbose: opts.verbose,
         timeout: 100000,
         transports: ['polling'],
